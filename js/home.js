2051afcf2ff69bd8fbbdf5d66e0f159e717c28a0
Maxime <maxime.saurin@gmail.com>
2021-04-05 21:11:06 +0200

    Div bug correct in home.html Script moved to home.js

--- js/home.js
+++ js/home.js
@@ -3,21 +3,21 @@
 
 
 
-// //NavBar
-// function hideIconBar() {
-//     var iconBar = document.getElementById("iconBar");
-//     var navigation = document.getElementById("navigation");
-//     iconBar.setAttribute("style", "display:none;");
-//     navigation.classList.remove("hide");
-// }
-//
-//
-// function showIconBar() {
-//     var iconBar = document.getElementById("iconBar");
-//     var navigation = document.getElementById("navigation");
-//     iconBar.setAttribute("style", "display:block;");
-//     navigation.classList.add("hide");
-// }
+//NavBar
+function hideIconBar() {
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:none;");
+    navigation.classList.remove("hide");
+}
+
+
+function showIconBar() {
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:block;");
+    navigation.classList.add("hide");
+}
 
 // $(function(){
 //     $("#controller1").click(function(){
