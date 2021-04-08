214f215d2511fb6164a30cd43a24f3cadcb40139
Maxime <maxime.saurin@gmail.com>
2021-04-08 19:44:46 +0200

    Adding sentences for the menu Modifying some properties in css file

--- js/navigation.js
+++ js/navigation.js
@@ -63,14 +63,14 @@ $(document).ready(function() {
 var quoteSentenceList = [];
 quoteSentenceList[0] = "Weakness Disgusts Me";
 quoteSentenceList[1] = "Giving up is what kills people";
-quoteSentenceList[2] = "";
-quoteSentenceList[3] = "";
-quoteSentenceList[4] = "";
-quoteSentenceList[5] = "";
-quoteSentenceList[6] = "";
-quoteSentenceList[7] = "";
-quoteSentenceList[8] = "";
-quoteSentenceList[9] = "";
+quoteSentenceList[2] = "This World Shall Know Pain";
+quoteSentenceList[3] = "The True Measure Of A Shinobi Is Not How He Lives, But How He Dies";
+quoteSentenceList[4] = "Those who cannot acknowledge themselves, will eventually fail";
+quoteSentenceList[5] = "That's my Flying Raijin Level 2!!";
+quoteSentenceList[6] = "Kaaaaa Meeeee Haaaaa Meeee HAAAAA!!!!!!!!";
+quoteSentenceList[7] = "Regain my Honour";
+quoteSentenceList[8] = "I'm Gonna Be Hokage and ignore Family Duties";
+quoteSentenceList[9] = "I will not allow my comrades to die";
 quoteSentenceList[10] = "";
 quoteSentenceList[11] = "";
 quoteSentenceList[12] = "";
@@ -79,23 +79,19 @@ quoteSentenceList[12] = "";
 var quoteCharacterList = [];
 quoteCharacterList[0] = "Madara, Naruto";
 quoteCharacterList[1] = "Alucard, Hellsing";
-quoteCharacterList[2] = "";
-quoteCharacterList[3] = "";
-quoteCharacterList[4] = "";
-quoteCharacterList[5] = "";
-quoteCharacterList[6] = "";
-quoteCharacterList[7] = "";
-quoteCharacterList[8] = "";
-quoteCharacterList[9] = "";
+quoteCharacterList[2] = "Nagato Uzumaki, Naruto";
+quoteCharacterList[3] = "Jiraya, Naruto";
+quoteCharacterList[4] = "Itachi, Naruto";
+quoteCharacterList[5] = "Minato, Naruto";
+quoteCharacterList[6] = "Goku, DBZ";
+quoteCharacterList[7] = "Zuko, Avatar";
+quoteCharacterList[8] = "Naruto, Naruto";
+quoteCharacterList[9] = "Kakashi, Naruto";
 quoteCharacterList[10] = "";
 quoteCharacterList[11] = "";
 quoteCharacterList[12] = "";
 
-
-// $('#quoteSentence').replaceWith($('#quoteCharacter'));
-
-// <h2 id="quoteSentence">"Weakness Disgusts Me"</h2>
-
+const actualQuoteNb = 10;
 
 // var rdNb = Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -107,13 +103,13 @@ function getRandomdNb(maxQuoteNb){
 
 
 function switchQuote(){
-    var rdNb = getRandomdNb(12);
+    var rdNb = getRandomdNb(actualQuoteNb-1);
     $('#quoteSentence').replaceWith('<h2 id="quoteSentence">"' + quoteSentenceList[rdNb] + '"</h2>');
     $('#quoteCharacter').replaceWith('<h3 id="quoteCharacter">- ' + quoteCharacterList[rdNb] + '</h3>');
 }
 
 
-// switchQuote();
+switchQuote();
 
 $('#switchButton').click(function() {
     switchQuote();
@@ -122,12 +118,6 @@ $('#switchButton').click(function() {
 $('.closeBtn').click(function() {
     switchQuote();
 });
-
-
-
-
-
-
 
 
 
