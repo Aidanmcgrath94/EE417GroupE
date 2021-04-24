5241adbb9514fcc46be59730b6dc1de2f373132d
Maxime <maxime.saurin@gmail.com>
2021-04-24 21:25:31 +0200

    Index (html + css + js)

---
+++ WebContent/js/index.js
@@ -0,0 +1,30 @@
+
+
+const test = new Letterize({
+    targets: ".sentence",
+    className: 'letter'
+});
+
+const animation = anime.timeline({
+    targets: test.listAll,
+    delay: anime.stagger(100, {
+        grid: [test.list[0].length, test.list.length],
+        from: "center"
+    }),
+    loop: true
+});
+
+animation
+    .add({
+        scale: 0.5
+    })
+    .add({
+        letterSpacing: "10px"
+    })
+    .add({
+        scale: 1
+    })
+    .add({
+        letterSpacing: "6px"
+    });
+
