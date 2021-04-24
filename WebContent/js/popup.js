dc7de23ba6d208b1a3420c4edd5054722f8ee0a0
Maxime <maxime.saurin@gmail.com>
2021-04-24 17:29:48 +0200

    update popup

--- WebContent/js/popup.js
+++ WebContent/js/popup.js
@@ -1,11 +1,20 @@
 
 
+
 function showPopUp(){
     popup.style.display = "block";
 }
 
 function closePopUp(){
     popup.style.display = "none";
+}
+
+//open the popup if previous page was signup
+var oldUrl = document.referrer;
+
+if(oldUrl.includes("signup")){
+    playSound('success')
+    showPopUp();
 }
 
 //close when click outside of the box
